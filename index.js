06ac03240c8827a2c98816bc9e35382138edb33c
Raul Menendez <orreryofcode@gmail.com>
2023-03-08 17:14:14 -0800

    Add new board submission functionality

--- index.js
+++ index.js
@@ -1,96 +1,86 @@
+class Board {
+  constructor(title) {
+    this.title = title;
+  }
+}
+
 // This is dummy data that will be replaced with form data
-const boardData = [
-  {
-    id: 1,
-    title: "Sample Board",
-    categories: {
-      todo: [
-        {
-          title: "Todo Task",
-          id: 1,
-          description: "This is a task",
-          subtasks: [1, 2, 3],
-          category: "todo",
-        },
-      ],
-      doing: [
-        {
-          title: "Doing Task",
-          id: 1,
-          description: "This is a task",
-          subtasks: [1, 2, 3],
-          category: "doing",
-        },
-      ],
-      done: [
-        {
-          title: "Done Task",
-          id: 1,
-          description: "This is a task",
-          subtasks: [1, 2, 3],
-          category: "done",
-        },
-      ],
-    },
-  },
-  {
-    id: 2,
-    title: "Sample Board #2",
-    categories: {
-      todo: [
-        {
-          title: "Todo Task",
-          id: 1,
-          description: "This is a task",
-          subtasks: [1, 2, 3],
-          category: "todo",
-        },
-      ],
-      doing: [],
-      done: [],
-    },
-  },
-];
+const boardData = [];
 
 const boardTitle = document.querySelector("h1");
 const boardList = document.querySelector(".nav__boardlist");
 const taskBoard = document.querySelector(".task__board");
 
+const newBoardBtn = document.querySelector(".sidebar__button-btn");
 const newTaskBtn = document.querySelector(".heading__button-btn");
-const modalWindow = document.querySelector(".modal__window-new-bg");
+const submitNewBoardForm = document.querySelector("#newBoardForm");
+const newBoardFormInput = document.querySelector("#newboardtitle");
+const newBoardModalWindow = document.querySelector(".modal__window-board");
+const newTaskModalWindow = document.querySelector(".modal__window-new-bg");
 
 class App {
   constructor() {
     this._populateSidebar();
-    newTaskBtn.addEventListener("click", this._openModal);
-    modalWindow.addEventListener("click", this._closeModal);
+    submitNewBoardForm.addEventListener("click", (e) => {
+      this._createNewBoard(e);
+      this._populateSidebar();
+      this._closeModal();
+    });
     boardList.addEventListener("click", this._populateBoardTracks.bind(this));
+
+    newBoardBtn.addEventListener("click", this._openModal);
+    newTaskBtn.addEventListener("click", this._openModal);
+    newTaskModalWindow.addEventListener("click", this._closeModal);
+    newBoardModalWindow.addEventListener("click", this._closeModal);
   }
 
-  _openModal() {
-    if (modalWindow.classList.contains("hidemodal")) {
-      modalWindow.classList.remove("hidemodal");
+  _openModal(e) {
+    if (e.target.id === "newBoardBtn") {
+      if (newBoardModalWindow.classList.contains("hidemodal")) {
+        newBoardModalWindow.classList.remove("hidemodal");
+      }
+    } else if ((e.target.id = "newTaskBtn")) {
+      if (newTaskModalWindow.classList.contains("hidemodal")) {
+        newTaskModalWindow.classList.remove("hidemodal");
+      }
     }
   }
 
   _closeModal(e) {
     if (
-      e.target === modalWindow &&
-      !modalWindow.classList.contains("hidemodal")
+      e.target === newBoardModalWindow &&
+      !newBoardModalWindow.classList.contains("hidemodal")
     ) {
-      modalWindow.classList.add("hidemodal");
+      newBoardModalWindow.classList.add("hidemodal");
+    } else if (
+      e.target === newTaskModalWindow &&
+      !newTaskModalWindow.classList.contains("hidemodal")
+    ) {
+      newTaskModalWindow.classList.add("hidemodal");
     }
+  }
+
+  // Creates new Board object from form input
+  _createNewBoard(e) {
+    e.preventDefault();
+    const newBoardTitle = newBoardFormInput.value;
+    const newBoard = new Board(newBoardTitle);
+    boardData.push(newBoard);
   }
 
   // Populate the list of boards in the sidebar
   _populateSidebar() {
     // boardData will be replaced by the elements we have in localstorage
-    boardData.map((board) => {
-      const boardTitle = document.createElement("li");
-      boardTitle.innerText = board.title;
-      boardTitle.classList.add("nav__board-item");
+
+    if (boardData.length > 0) {
+      let boardTitle;
+      boardData.map((board) => {
+        boardTitle = document.createElement("li");
+        boardTitle.innerText = board.title;
+        boardTitle.classList.add("nav__board-item");
+      });
       boardList.append(boardTitle);
-    });
+    }
   }
 
   // Populate the task tracks in the main section
@@ -99,7 +89,7 @@ class App {
 
     const target = e.target.innerText;
     boardData.forEach((element) => {
-      if (element.title === target) {
+      if (element.title && element.title === target) {
         boardTitle.innerText = element.title;
         for (const key in element.categories) {
           if (element.categories[key].length > 0) {
